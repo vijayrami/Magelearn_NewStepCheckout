b229d5806a10acbb5bb26dfa69d86bb2af519f09
Vijay Rami <vijaymrami@gmail.com>
2021-12-02 11:57:04 +0530

    Update new-step.js

--- view/frontend/web/js/view/new-step.js
+++ view/frontend/web/js/view/new-step.js
@@ -25,12 +25,22 @@ define([
             this._super();
 
             stepNavigator.registerStep(
+            	// step code will be used as step content id in the component template
                 'new_step',
+                // step alias
                 null,
+                // step title value
                 $t('Checkout Products'),
+                // observable property with logic when display step or hide step
                 this.isVisible,
 
                 _.bind(this.navigate, this),
+                /**
+                 * sort order value
+                 * 'sort order value' < 10: step displays before shipping step;
+                 * 10 < 'sort order value' < 20 : step displays between shipping and payment step
+                 * 'sort order value' > 20 : step displays after payment step
+                 */
                 1
             );
 
@@ -38,8 +48,7 @@ define([
         },
 
         navigate: function () {
-            var self = this;
-            self.isVisible(true);
+            this.isVisible(true);
         },
 
         /**
